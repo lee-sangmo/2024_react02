b06f56a039628a38e67f8d3679909fda6f976e37
lee-sangmo <cochiny@gmail.com>
2024-11-22 17:46:00 +0900

    useReducer_241122_05

--- src/components/step15/ReducerTest02.jsx
+++ src/components/step15/ReducerTest02.jsx
@@ -3,29 +3,42 @@ import React, { useReducer, useState } from 'react';
 
 function reducer(state, action) {
   switch (action.type) {
-    case "input":
+    case "inCash":
       return state + action.payload;
-    case "output":
-      return state >= action.payload ? state - action.payload : alert("잔고가 부족합니다");
+    case "outCash":
+      return state - action.payload;
     default:
       return state;
   }
 }
 
 function ReducerTest02(props) {
-  const [number, setNumber] = useState(0)           // 입력 금액 관리
-  const [money, dispatch] = useReducer(reducer, 1000)  // 잔고 관리
+  const [number, setNumber] = useState(0)               // input 금액 관리
+  const [money, dispatch] = useReducer(reducer, 1000)   // 잔고 관리
   return (
     <div>
       <h2>Star Bank에 오신 것을 환영합니다</h2>
       <p>잔고 : {money}원</p>
       <input type='number'
         value={number}
-        onChange={(e) => setNumber(e.target.value)}
+        onChange={(e) => {
+          const value = parseInt(e.target.value) || 0;
+          setNumber(value < 0 ? 0 : value);               // 음수 방지
+        }}
         step="1000"
       />
-      <button onClick={() => dispatch({type: "input", payload: Number(number) })}>예금</button>
-      <button onClick={() => dispatch({type: "output", payload: Number(number) })}>출금</button>
+      <button onClick={() => {
+        dispatch({ type: "inCash", payload: number });
+        setNumber(0);
+      }}>예금</button>
+      <button onClick={() => {
+        if (number > money) {
+          alert("잔고가 부족합니다")
+        } else {
+          dispatch({ type: "outCash", payload: number });
+          setNumber(0);
+        }
+      }}>출금</button>
     </div>
   );
 }
